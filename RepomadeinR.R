c715ad5dd16534acefb3394c46b83c77ae6bb935
Eyvind Johansson <eyvindj@gmail.com>
2024-11-18 22:00:54 +0100

    Script and lock file updated to create ggplot with counts

--- RepomadeinR.R
+++ RepomadeinR.R
@@ -1,3 +1,23 @@
-install.packages("usethis")
+install.packages("usethis", "tidyverse","renv")
 library(usethis)
 library(renv)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(forcats)
+library(GGally)
+
+df <- as_tibble(iris)
+
+ggpairs(iris, aes(color = Species))
+
+# Summarize the data
+summary_data <- iris %>%
+  group_by(Petal.Length, Species) %>%
+  summarise(Count = n(), .groups = "drop")
+
+# Use geom_col() to plot
+ggplot(summary_data, aes(x = Petal.Length, y = Count, fill = Species)) +
+  geom_col()
+
+  
